09b7e36ec5b6ec77b1332aff506a86425ec58460
David <david.dmr@gmail.com>
2017-06-09 00:17:03 -0300

    Creating Class to manager colors of the app

---
+++ app/src/main/java/com/david/jumper/graphic/Colors.java
@@ -0,0 +1,18 @@
+package com.david.jumper.graphic;
+
+import android.graphics.Paint;
+
+/**
+ * Created by david on 08/06/2017.
+ */
+public class Colors {
+
+    public static Paint getBirdColor() {
+
+        Paint paint = new Paint();
+        paint.setColor(0xFFFF0000);
+
+        return paint;
+    }
+
+}
