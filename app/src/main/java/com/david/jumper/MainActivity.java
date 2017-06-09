1fc6933f0345fbc52cb47f76f15e8df283ee3d89
David <david.dmr@gmail.com>
2017-06-09 00:23:52 -0300

    Implementation of the main act

--- app/src/main/java/com/david/jumper/MainActivity.java
+++ app/src/main/java/com/david/jumper/MainActivity.java
@@ -2,12 +2,35 @@ package com.david.jumper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.david.jumper.engine.Game;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout container = (FrameLayout) findViewById(R.id.container);
+
+        game = new Game(this);
+        container.addView(game);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        game.start();
+        new Thread(game).start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        game.pause();
     }
 }
